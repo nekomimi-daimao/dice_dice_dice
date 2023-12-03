ca406d3c5f25a8077615746a8b7d7bc7807e5fc7
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2023-12-03 18:23:39 +0900

    mainを作成したものに入れ替える

---
+++ lib/widget/root_view.dart
@@ -0,0 +1,33 @@
+import 'package:dice_dice_dice/provider/roll_dice.dart';
+import 'package:dice_dice_dice/widget/roll_result.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'dice_selecter.dart';
+import 'grid_count.dart';
+
+class RootView extends HookConsumerWidget {
+  const RootView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final roll = ref.watch(rollDiceProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("d3"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RollResult(roll: roll),
+            const DiceSelector(),
+            const GridCount(),
+          ],
+        ),
+      ),
+    );
+  }
+}
