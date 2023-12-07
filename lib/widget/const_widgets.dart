fb438252a42d58cb1c604ed42b4007fc06754830
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2023-12-08 00:05:48 +0900

    widgetの長さをconstにまとめる

--- lib/widget/const_widgets.dart
+++ lib/widget/const_widgets.dart
@@ -1,4 +1,9 @@
 class ConstWidgets {
+  static const double borderWidth = 2;
+
   static const double diceViewWidth = 80;
   static const double diceViewHeight = diceViewWidth;
+
+  static const double rollViewWidth = 200;
+  static const double rollViewHeight = 200;
 }
--- lib/widget/roll_view.dart
+++ lib/widget/roll_view.dart
@@ -1,3 +1,4 @@
+import 'package:dice_dice_dice/widget/const_widgets.dart';
 import 'package:dice_dice_dice/widget/dice_view.dart';
 import 'package:flutter/material.dart';
 import 'package:dice_dice_dice/model/roll.dart';
@@ -9,15 +10,14 @@ class RollView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> results = [];
-    results.addAll(roll.result.map((e) => Text(e.toString())));
-
     return Container(
-        width: 200,
-        constraints: const BoxConstraints(minHeight: 200),
+        width: ConstWidgets.rollViewWidth,
+        constraints:
+            const BoxConstraints(minHeight: ConstWidgets.rollViewHeight),
         padding: const EdgeInsets.all(5.0),
         decoration: BoxDecoration(
-          border: Border.all(color: Colors.green),
+          border:
+              Border.all(color: Colors.green, width: ConstWidgets.borderWidth),
           borderRadius: BorderRadius.circular(10),
         ),
         child: Column(
@@ -29,7 +29,7 @@ class RollView extends StatelessWidget {
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Wrap(
-                  children: results,
+                  children: roll.result.map((e) => Text(e.toString())).toList(),
                 ),
               ),
             ),
