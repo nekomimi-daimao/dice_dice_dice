08ba595a7e85c504dd4422063c16300890c1e1ce
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2023-12-07 22:40:53 +0900

    要素が多すぎる場合はスクロールする

--- lib/widget/roll_view.dart
+++ lib/widget/roll_view.dart
@@ -10,29 +10,37 @@ class RollView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> results = [];
-    results.add(Text("${roll.sum()} = "));
     results.addAll(roll.result.map((e) => Text(e.toString())));
 
     return Container(
         width: 200,
-        height: 200,
+        constraints: const BoxConstraints(minHeight: 200),
         padding: const EdgeInsets.all(5.0),
         decoration: BoxDecoration(
           border: Border.all(color: Colors.green),
           borderRadius: BorderRadius.circular(10),
         ),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            Text(roll.sum().toString()),
+            Container(
+              constraints: const BoxConstraints(maxHeight: 400),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Wrap(
+                  children: results,
+                ),
+              ),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 DiceView(dice: roll.dice),
                 const Icon(Icons.close),
                 Text(roll.count.toString()),
               ],
             ),
-            Wrap(
-              children: results,
-            )
           ],
         ));
   }
