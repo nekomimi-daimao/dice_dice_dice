8fd7573cc66f464275536ed1eb3c83e039668adf
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2023-12-20 01:39:10 +0900

    ダイスの結果を選択可能にする

--- lib/widget/roll_view.dart
+++ lib/widget/roll_view.dart
@@ -20,71 +20,73 @@ class RollView extends HookConsumerWidget {
       result = result.reversed.toList();
     }
 
-    return Card(
-      color: Theme.of(context).colorScheme.primaryContainer,
-      child: Container(
-        width: ConstWidgets.rollViewWidth,
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Card(
-              shape: const CircleBorder(),
-              color: Theme.of(context).colorScheme.primary,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  roll.sum().toString(),
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onPrimary,
+    return SelectionArea(
+      child: Card(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Container(
+          width: ConstWidgets.rollViewWidth,
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Card(
+                shape: const CircleBorder(),
+                color: Theme.of(context).colorScheme.primary,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    roll.sum().toString(),
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              constraints: const BoxConstraints(maxHeight: 70),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Wrap(
-                  children: result
-                      .map(
-                        (e) => Text(
-                          "$e, ",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer,
+              Container(
+                constraints: const BoxConstraints(maxHeight: 70),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Wrap(
+                    children: result
+                        .map(
+                          (e) => Text(
+                            "$e, ",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .onPrimaryContainer,
+                            ),
                           ),
-                        ),
-                      )
-                      .toList(),
+                        )
+                        .toList(),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  roll.count.toString(),
-                  style: TextStyle(
-                    fontSize: 20,
+              const SizedBox(
+                height: 4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    roll.count.toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onPrimaryContainer,
+                    ),
+                  ),
+                  Icon(
+                    Icons.close,
                     color: Theme.of(context).colorScheme.onPrimaryContainer,
                   ),
-                ),
-                Icon(
-                  Icons.close,
-                  color: Theme.of(context).colorScheme.onPrimaryContainer,
-                ),
-                DiceView(dice: roll.dice),
-              ],
-            ),
-          ],
+                  DiceView(dice: roll.dice),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
